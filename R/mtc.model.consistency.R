d8d3efd6ce69c9741037187033c8d362e08c452d
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-02-18 00:00:00 +0000

    version 0.2

---
+++ R/mtc.model.consistency.R
@@ -0,0 +1,33 @@
+# Master function to generate consistency models
+mtc.model.consistency <- function(network, factor, n.chain) {
+    style.tree <- function(tree) {
+        tree <- set.edge.attribute(tree, 'arrow.mode', value=2)
+        tree <- set.edge.attribute(tree, 'color', value='black')
+        tree <- set.edge.attribute(tree, 'lty', value=1)
+        tree
+    }
+
+    model <- list(
+        type = 'Consistency',
+        network = network,
+        tree = style.tree(minimum.diameter.spanning.tree(mtc.network.graph(network))),
+        n.chain = n.chain,
+        var.scale = factor
+    )
+
+    if ('responders' %in% colnames(network$data)) {
+        model$likelihood = 'binom'
+        model$link = 'logit'
+    } else if ('mean' %in% colnames(network$data)) {
+        model$likelihood = 'normal'
+        model$link = 'identity'
+    }
+
+    model$om.scale <- guess.scale(model)
+    model$code <- mtc.model.code(model)
+    model$data <- mtc.model.data(model)
+    model$inits <- mtc.init(model)
+    class(model) <- "mtc.model"
+
+    model
+}
