79e8d56d332f24a17ff874140ba6784592eee389
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2013-02-19 07:35:53 +0000

    version 0.2

---
+++ R/mtc.model.R
@@ -0,0 +1,55 @@
+## mtc.model class methods
+
+mtc.model <- function(network, type="Consistency", factor=2.5, n.chain=4) {
+    typeMap <- c(
+        'Consistency'='Consistency',
+        'consistency'='Consistency',
+        'cons'='Consistency')
+
+    if (is.na(typeMap[type])) {
+        stop(paste(type, 'is not an MTC model type.'))
+    }
+    type <- typeMap[type]
+
+    if (type == 'Consistency') {
+        mtc.model.consistency(network, factor=factor, n.chain=n.chain)
+    }
+}
+
+print.mtc.model <- function(x, ...) {
+    cat("MTC ", x$type, " model: ", x$description, "\n", sep="")
+}
+
+summary.mtc.model <- function(object, ...) {
+    list("Description"=paste("MTC ", object$type, " model: ", object$description, sep=""), 
+         "Parameters"=mtc.basic.parameters(object))
+}
+
+plot.mtc.model <- function(x, layout=igraph::layout.circle, ...) {
+    igraph::plot.igraph(mtc.model.graph(x), layout=layout, ...)
+}
+
+mtc.model.graph <- function(model) { 
+    if (model$type == 'Consistency') {
+        comparisons <- mtc.comparisons(model$network)
+        g <- model$tree
+        g <- g + edges(as.vector(unlist(non.edges(g, comparisons))), arrow.mode=0, lty=1, color="grey")
+        g
+    }
+}
+
+# filters list of comparison by edges that are not yet present in graph g 
+non.edges <- function(g, comparisons) { 
+    sapply(1:nrow(comparisons), function(i) {
+        x <- c(comparisons$t1[i], comparisons$t2[i])
+        if (are.connected(g, x[1], x[2]) || are.connected(g, x[2], x[1])) c() else x
+    })
+}
+
+mtc.basic.parameters <- function(model) {
+    tree <- model$tree
+    sapply(E(tree), function(e) {
+        v <- get.edge(tree, e)
+        paste("d", V(tree)[v[1]]$name, V(tree)[v[2]]$name, sep=".")
+    })
+}
---
+++ R/mtc.result.R
@@ -0,0 +1,39 @@
+## mtc.result class methods
+print.mtc.result <- function(x, ...) {
+    cat("MTC ", x$model$type, " results: ", x$model$description, sep="")
+    print(x$samples)
+}
+
+summary.mtc.result <- function(object, ...) {
+    summary(object$samples)
+}
+
+plot.mtc.result <- function(x, ...) {
+    plot(x$samples, ...)
+}
+
+forest.mtc.result <- function(x, ...) { 
+    quantiles <- summary(x)$quantiles 
+    stats <- quantiles[grep("^d\\.", rownames(quantiles)),]
+    if(class(stats) == "numeric") { # Selecting a single row returns a numeric 
+        stats <- as.matrix(t(stats))
+        row.names(stats) <- row.names(quantiles)[[1]]
+    }
+    groups <- extract.comparisons(rownames(quantiles))[,1]
+    group.names <- unique(groups)
+    group.labels <- rep("", length(group.names))
+    #group.labels <- paste("Relative to ", group.names)
+    names(group.labels) <- group.names
+    data <- data.frame(
+        id=rownames(stats),
+        pe=stats[,3], ci.l=stats[,1], ci.u=stats[,5],
+        group=groups, style="normal")
+    blobbogram(data,
+        columns=c(), column.labels=c(),
+        id.label="Comparison", ci.label="Odds Ratio (95% CrI)", log.scale=TRUE,
+        grouped=length(group.labels)>1, group.labels=group.labels)
+}
+
+as.mcmc.list.mtc.result <- function(x, ...) {
+    x$samples
+}
